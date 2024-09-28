3d6a93804bd99daf2e71daf4e4ec14a493a176de
abhinav <abhinav312003@gmail.com>
2024-09-28 21:54:31 +0530

    commit

--- src/components/shared/GridPostLists.tsx
+++ src/components/shared/GridPostLists.tsx
@@ -1,7 +1,5 @@
 import { Models } from "appwrite";
 import { Link } from "react-router-dom";
-
-
 import { useUserContext } from "@/context/AuthContext";
 import PostStats from "./PostStats";
 
@@ -24,7 +22,7 @@ const GridPostList = ({
         <li key={post.$id} className="relative min-w-80 h-80">
           <Link to={`/posts/${post.$id}`} className="grid-post_link">
             <img
-              src={post.imageUrl}
+              src={post?.imageUrl || "/assets/icons/profile-placeholder.svg"}
               alt="post"
               className="h-full w-full object-cover"
             />
@@ -35,16 +33,16 @@ const GridPostList = ({
               <div className="flex items-center justify-start gap-2 flex-1">
                 <img
                   src={
-                    post.creator.imageUrl ||
+                    post?.creator?.imageUrl ||
                     "/assets/icons/profile-placeholder.svg"
                   }
                   alt="creator"
                   className="w-8 h-8 rounded-full"
                 />
-                <p className="line-clamp-1">{post.creator.name}</p>
+                <p className="line-clamp-1">{post?.creator?.name || "Unknown"}</p>
               </div>
             )}
-            {showStats && <PostStats post={post} userId={user.id} />}
+            {showStats && <PostStats post={post} userId={user?.id} />}
           </div>
         </li>
       ))}

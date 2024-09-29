6e4aae7e27c725087caa04bfcecbce2952df19c5
abhinav <abhinav312003@gmail.com>
2024-09-29 09:42:56 +0530

    commit

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -11,7 +11,7 @@ const AuthLayout = () => {
           <section className='flex flex-1 justify-center items-center flex-col py-10 bg-black'>
             <Outlet />
           </section>
-          <img src="/public/assets/images/side-img.svg"
+          <img src="/assets/images/side-img.svg"
            alt=""
            className='hidden xl:block h-screen w-1/2 object-cover bg-no-repeat'
            />
